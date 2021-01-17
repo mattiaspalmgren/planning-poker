2260426997d45a3a3bddbb26b092da02a86e4574
Mattias Palmgren <mattiaspalmgren92@gmail.com>
2021-01-17 16:26:30 +0100

    Format all files with prettier

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const element = screen.getByText(/Sana Labs/i);
   expect(element).toBeInTheDocument();
